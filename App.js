6b16521d48e05e98c5c549d8792e342cadfe6bed
torihcotton <54193943+torihcotton@users.noreply.github.com>
2019-09-18 17:47:55 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
-import
 
 class App extends Component {
 
   constructor(props) {
      super(props);
-     this.state = { name: 'example.stl', weight: 90, volume: 180, bmi: 27, message: '', optimalweight: '', time: new Date().toLocaleTimeString() };
+     this.state = { name: 'example.stl', volume: 180, total: 27, message: '', optimalweight: '', time: new Date().toLocaleTimeString() };
      this.submitMe = this.submitMe.bind(this);
      this.volumechange = this.volumechange.bind(this);
-     this.weightchange = this.weightchange.bind(this);
      this.change = this.change.bind(this);  
      this.ticker = this.ticker.bind(this); 
      this.blur = this.blur.bind(this); 
-     this.calculateBMI = this.calculateBMI.bind(this); 
+     this.calculatetotal = this.calculateBMI.bind(this); 
+     this.volumechange = this.volumechange.bind(this);
   }
 
 
@@ -25,33 +24,26 @@ class App extends Component {
   blur(e){
    this.calculateBMI();
   }
-   weightchange(e){
-    this.setState({weight: e.target.value});
-    e.preventDefault();
-  }
 
   calculateBMI(){
 
       var volumeSquared = (this.state.volume/100  * this.state.volume/100);
-      var bmi = this.state.weight / volumeSquared;
-      var low = Math.round(18.5 * volumeSquared);                                                         
-      var high = Math.round(24.99 * volumeSquared);    
+      var total= this.state.weight * volumeSquared;
       var message = "";
-      if( bmi >= 18.5  && bmi <= 24.99 ){
+      if( total >= 18.5  && total <= 24.99 ){
           message = "You are in a healthy weight range";
       }
-      else if(bmi >= 25 && bmi <= 29.9){
+      else if(total >= 25 && total <= 29.9){
         message = "You are overweight";
       }
-      else if(bmi >= 30){
+      else if(total >= 30){
           message ="You are obese";
       }
-      else if(bmi < 18.5){
+      else if(total < 18.5){
         message = "You are under weight";
       }
-      this.setState({message: message});  
-      this.setState({optimalweight: "Your suggested weight range is between "+low+ " - "+high});    
-      this.setState({bmi: Math.round(bmi * 100) / 100});   
+      this.setState({message: message});     
+      this.setState({bmi: Math.round(total * 100) / 100});   
 
   }
 
@@ -66,61 +58,3 @@ class App extends Component {
  
   componentDidMount(){
     setInterval(this.ticker, 60000);
-  }
-
-  change(e){
-    e.preventDefault();
-    console.log(e.target);
-    this.setState({name: e.target.value});
-  }
-
-  render() {
-    return (
-      <div className="App">
-       <div style={{ backgroundColor: "#333f48", width: "300px", maxHeight: "250px",}}>
-	 <div className="App-header">
-          <h2 style={{ padding: "10px 20px", textAlign: "right", color: "#bf5700"}}>FAB LAB cost calculator</h2>
-        </div></div>
-          
-          
- 
-       <h3 style={{ padding: "10px 50px", textAlign: "center", color: "#bf5700"}}>Select your 3d printing material:
-	
-<label> Input values: </label></h3>
-       
-
-<h4 style={{ padding: "10px 20px", textAlign: "center", color: "#bf5700", headersize: "large"}}>Input Values:</h4>
-	
- <h5 style={{ padding: "15px 325px", textAlign: "justified", color: "#bf5700"}}>
-
-  <form onSubmit={this.submitMe}>
-            <label>
-             Enter file name:
-            </label>
-            <input type="text" name="name" value={this.state.name} onBlur={this.blur} onChange={this.change}   />
-             <label>
-             Enter the volume of your print: 
-            </label>
-            <input type="text" name="volume" value={this.state.volume} onBlur={this.blur} onChange={this.volumechange}   />
-             <label>
-             Enter your weight in kg : 
-            </label>
-            <input type="text" name="weight" value={this.state.weight} onChange={this.weightchange}    />
-            <label>{this.state.checked} 
-
-
-
-It is currently  {this.state.time}. Your print, {this.state.name}, will cost. Your BMI is {this.state.bmi} </label>
-              <label>{this.state.message}</label>
-              <label>{this.state.optimalweight}</label>
-             
-            <input type="submit" value="Submit"/>
-          </form>
-</h5>
-
-      </div>
-
-    );
-  }
-}
-export default App;
